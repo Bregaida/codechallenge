c91fa42ad5555c7895045e35ff5700f136df7df1
Eduardo Bregaida <eduardo.bregaida@gmail.com>
2014-04-15 23:56:44 -0300

    Teste da Inserção da Rota

--- src/test/java/br/com/brasilct/codechallenge/service/RouteServiceTest.java
+++ src/test/java/br/com/brasilct/codechallenge/service/RouteServiceTest.java
@@ -1,5 +1,7 @@
 package br.com.brasilct.codechallenge.service;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import br.com.brasilct.codechallenge.domain.route.RouteService;
@@ -9,4 +11,9 @@ public class RouteServiceTest extends AbstractServiceTestSupport {
     @Autowired
     private RouteService service;
 
+    @Test
+    public void deveriaBuscarDadosInseridos() {
+	service.loadData();
+	Assert.assertTrue(service.findAll().size() > 0);
+    }
 }
